4d361e8a9bcb5008732823d45f96e697e618d198
syntax <tfeholland@gmail.com>
2025-02-09 16:28:49 +0000

    add ray class

---
+++ colour.h
@@ -0,0 +1,18 @@
+#ifndef COLOUR_H
+#define COLOUR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using colour = vec3;
+
+
+void write_colour(std::ostream& out, const colour& pixel_colour) {
+    // x corresponds to red, y to green, z to blue
+    out << static_cast<int>(255.999 * pixel_colour.x()) << ' '
+        << static_cast<int>(255.999 * pixel_colour.y()) << ' '
+        << static_cast<int>(255.999 * pixel_colour.z()) << '\n';
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "colour.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
@@ -14,15 +17,8 @@ int main() {
     for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << image_height - j << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_colour = colour(double(i) / (image_width-1), double(j) / (image_height-1), 0);
+            write_colour(std::cout, pixel_colour);
         }
     }
 
