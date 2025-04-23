9bbc073761d3a7044a96db6b1a2d6c84c6ed51ff
syntax <tfeholland@gmail.com>
2025-04-23 12:36:00 +0100

    new cpp version, use unique pointer instead of shared pointer

--- hittable_list.h
+++ hittable_list.h
@@ -7,16 +7,19 @@
 
 class hittable_list : public hittable {
   public:
-    std::vector<shared_ptr<hittable>> objects;
+    std::vector<std::unique_ptr<hittable>> objects;
 
-    hittable_list() {}
-    hittable_list(shared_ptr<hittable> object) { add(object); }
+    hittable_list() = default;
+    explicit hittable_list(std::unique_ptr<hittable> object) {
+        add(std::move(object));
+    }
 
     void clear() { objects.clear(); }
 
-    void add(shared_ptr<hittable> object) {
-        objects.push_back(object);
+    void add(std::unique_ptr<hittable> object) {
+        objects.push_back(std::move(object));
     }
+
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         hit_record temp_rec;
@@ -24,7 +27,7 @@ class hittable_list : public hittable {
         auto closest_so_far = ray_t.max;
 
         for (const auto& object : objects) {
-            if (object->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
+            if (object->hit(r, {ray_t.min, closest_so_far}, temp_rec)) {
                 hit_anything = true;
                 closest_so_far = temp_rec.t;
                 rec = temp_rec;
--- main.cpp
+++ main.cpp
@@ -6,64 +6,77 @@
 #include "hittable_list.h"
 #include "sphere.h"
 
+#include <memory>
+
 int main() {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(colour(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    // ground
+    world.add(std::make_unique<sphere>(
+        point3(0, -1000, 0), 1000,
+        std::unique_ptr<material>( new lambertian(colour(0.5, 0.5, 0.5)) )
+    ));
 
-    // reduce this for time being so i dont have to wait forever
+    // random small spheres
     for (int a = -3; a < 3; a++) {
         for (int b = -3; b < 3; b++) {
             auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
             if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
-
                 if (choose_mat < 0.8) {
                     // diffuse
                     auto albedo = colour::random() * colour::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    world.add(std::make_unique<sphere>(
+                        center, 0.2,
+                        std::unique_ptr<material>( new lambertian(albedo) )
+                    ));
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = colour::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    auto fuzz   = random_double(0, 0.5);
+                    world.add(std::make_unique<sphere>(
+                        center, 0.2,
+                        std::unique_ptr<material>( new metal(albedo, fuzz) )
+                    ));
                 } else {
                     // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    world.add(std::make_unique<sphere>(
+                        center, 0.2,
+                        std::unique_ptr<material>( new dielectric(1.5) )
+                    ));
                 }
             }
         }
     }
 
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    world.add(std::make_unique<sphere>(
+        point3(0, 1, 0), 1.0,
+        std::unique_ptr<material>( new dielectric(1.5) )
+    ));
 
-    auto material2 = make_shared<lambertian>(colour(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+    world.add(std::make_unique<sphere>(
+        point3(-4, 1, 0), 1.0,
+        std::unique_ptr<material>( new lambertian(colour(0.4, 0.2, 0.1)) )
+    ));
 
-    auto material3 = make_shared<metal>(colour(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+    world.add(std::make_unique<sphere>(
+        point3(4, 1, 0), 1.0,
+        std::unique_ptr<material>( new metal(colour(0.7, 0.6, 0.5), 0.0) )
+    ));
 
     camera cam;
-
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 1200;
-    cam.samples_per_pixel = 10; // when this is set at 500, it will take longer.
+    cam.samples_per_pixel = 20; // when this is set at 500, it will take longer.
     cam.max_depth         = 50;
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
-
+    cam.vfov          = 20;
+    cam.lookfrom      = point3(13, 2, 3);
+    cam.lookat        = point3(0, 0, 0);
+    cam.vup           = vec3(0, 1, 0);
     cam.defocus_angle = 0.6;
-    cam.focus_distance    = 10.0;
+    cam.focus_distance = 10.0;
 
     cam.render(world);
 }
